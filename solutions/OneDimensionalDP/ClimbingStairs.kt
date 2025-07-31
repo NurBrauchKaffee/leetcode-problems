d3fbb4767322713338bea8db9e8e7ded82ba0e4f
JustNeedCoffee <JustNeedCoffee@list.ru>
2025-07-31 18:16:42 +0300

    added 1 problem (Climbing Stairs) on Kotlin; all "Easy" problems are done

---
+++ solutions/OneDimensionalDP/ClimbingStairs.kt
@@ -0,0 +1,33 @@
+package solutions.OneDimensionalDP
+
+fun climbStairs(n: Int): Int {
+    var prev = 1
+    var cur = 0
+
+    for (i in 0..n) {
+        val next = cur + prev
+        prev = cur
+        cur = next
+    }
+
+    return cur
+}
+
+fun showcase(testcases: List<Int>) {
+    for (testcase in testcases) {
+        println("number of ways: ${climbStairs(testcase)}, steps: $testcase")
+    }
+}
+
+fun main() {
+    val testcases = listOf(
+        2,
+        3,
+        4,
+        5,
+        6,
+        45
+    )
+
+    showcase(testcases)
+}
